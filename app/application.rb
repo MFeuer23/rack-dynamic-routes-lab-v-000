8c18e394ecbf11a006e99b9bffa3c50012b9834b
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-02 18:05:35 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+ 
+  @@items = []
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+      item_title = req.path.split("/items/").last
+      if item = @@items.find{|i| i.title == item_title}
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Path Not Found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
+end
