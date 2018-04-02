88a1b41249f1ea30ca1217d16285c7fb71a4c1b0
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-02 18:07:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-      item_title = req.path.split("/items/").last
-      if item = @@items.find{|i| i.title == item_title}
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else
         resp.write "Item not found"
